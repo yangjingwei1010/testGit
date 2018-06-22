49c41cf0da4d9fd098efbe4d1ead3d384c5ba965
juswill <juswill.yang@100tal.com>
2018-06-22 09:18:17 +0800

    张三测试

--- testframework/testframework/ViewController.m
+++ testframework/testframework/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  //张三测试
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
